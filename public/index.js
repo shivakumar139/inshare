05157f6ee5503154251aa48e11b9ca643272d8de
shivarajput1 <shivarajput.pp98@gmail.com>
2022-06-26 17:48:53 +0530

    changes in base url

--- public/index.js
+++ public/index.js
@@ -15,8 +15,8 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-// const baseURL = "https://innshare.herokuapp.com";
-const baseURL = "http://localhost:7000";
+const baseURL = "https://inshare5.herokuapp.com";
+// const baseURL = "http://localhost:7000";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
